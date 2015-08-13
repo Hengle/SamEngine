f567b765681d328dfc4b75b2a6a436135d51f0d1
leafnsand <jiangsongyou@gmail.com>
2015-08-13 18:17:49 +0800

    io module url

--- engine/modules/io/filesystem/url.h
+++ engine/modules/io/filesystem/url.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <string>
+
 namespace sam
 {
     class url
     {
 	public:
-		const std::string &get_filesystem() const { return ""; }
+		url();
+
+		url(const char *c_str);
+
+		url(const std::string &string);
+
+		url(url &other);
+
+		url(url &&other);
+
+		const std::string &get_filesystem();
+
+	protected:
+		std::string raw_string;
+		std::string fs_name;
     };
 }
