a83b1e4236695fb68807effdcbccb2b145c03184
leafnsand <jiangsongyou@gmail.com>
2015-08-18 16:27:38 +0800

    indent with 4 space

--- sample/Hello.cpp
+++ sample/Hello.cpp
@@ -20,29 +20,29 @@ public:
 
 app::state Hello::initialize()
 {
-	graphics::initialize(graphics::param::window(800, 600, "Sam"));
+    graphics::initialize(graphics::param::window(800, 600, "Sam"));
 
-	io::initialize();
+    io::initialize();
 
-	storage::initialize("C:\\Users\\leafnsand\\Desktop\\");
+    storage::initialize("C:\\Users\\leafnsand\\Desktop\\");
 
-	io::set_filesystem("storage", storage_filesystem::creator);
+    io::set_filesystem("storage", storage_filesystem::creator);
 
-	io::read("storage:test.txt", [](event_ptr &e)
-	{
-		auto event = std::dynamic_pointer_cast<io_request_read_event>(e);
+    io::read("storage:test.txt", [](event_ptr &e)
+    {
+        auto event = std::dynamic_pointer_cast<io_request_read_event>(e);
 
-		log::debug("%s\n", (const char *)event->get_data()->get_buffer());
+        log::debug("%s\n", (const char *) event->get_data()->get_buffer());
 
-		io::write("storage:copy.txt", event->get_data());
-	});
+        io::write("storage:copy.txt", event->get_data());
+    });
 
-	return app::initialize();
+    return app::initialize();
 }
 
 app::state Hello::running()
 {
-	graphics::present();
+    graphics::present();
     return graphics::should_quit() ? state::finalize : state::running;
 }
 
